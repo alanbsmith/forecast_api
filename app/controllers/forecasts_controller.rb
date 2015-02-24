ec878f75714419aba12e897972f6f4923cc06d33
Alan Smith <a.bax.smith@gmail.com>
2015-02-23 19:38:03 -0700

    adds logic for unknown locations

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -2,10 +2,14 @@ class ForecastsController < ApplicationController
 
   def show
     location = Geocoder.search(params[:id]).first
-    lat      = location.geometry['location']['lat']
-    long     = location.geometry['location']['lng']
-    forecast = Forecast.get_forecast(lat,long)
-    render :json => forecast.currently
+    if location.nil?
+      render :json => "Location not found."
+    else
+      lat      = location.geometry['location']['lat']
+      long     = location.geometry['location']['lng']
+      forecast = Forecast.get_forecast(lat,long)
+      render :json => forecast.currently
+    end
   end
 
 end
